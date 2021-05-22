060f7a196cb8c63b37a3c2eee4173fc7d7c04982
RuShHaar <stefan.ockert@gmx.net>
2021-05-22 17:45:03 +0200

    Anlage der ersten Probs - gerne verändern :) - leadId hinzugefügt

--- src/main/java/main/java/Leads.java
+++ src/main/java/main/java/Leads.java
@@ -5,6 +5,7 @@ public class Leads {
     private String telNumber; // <-- int oder String? Falls jemand sowas wie +49 eintippt?
     private String email;
     private Account company;
+    private int leadId;
 
 
 }
