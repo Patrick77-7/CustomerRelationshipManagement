fb134f948155598127c9ea9e384cade5a4642994
Maxi Kraft <kraft.maxi@gmail.com>
2021-05-23 14:48:07 +0200

    First setup

--- src/main/java/main/java/Contact.java
+++ src/main/java/main/java/Contact.java
@@ -2,11 +2,54 @@ package main.java;
 
 public class Contact {
 
-    private Integer contactId;
+    private String contactId;
     private String name;
     private String phoneNumber;
     private String email;
     private String companyName;
 
+    private static Integer idCounterC = 0;
+
+    public Contact(Lead lead){
+
+        this.name = lead.getName();
+        this.email = lead.getEmail();
+        this.companyName = lead.getCompanyName();
+        this.phoneNumber = lead.getPhoneNumber();
+        setContactId();
+    }
+
+    public static Integer createID() {
+        return idCounterC++;
+    }
+
+    public void setContactId() {
+        Integer newId = createID();
+        this.contactId = "C " + newId;
+    }
+
+    public void setContactId(String contactId) {
+        this.contactId = contactId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getContactId() {
+        return contactId;
+    }
 
 }
