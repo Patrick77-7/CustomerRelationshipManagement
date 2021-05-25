8c5175957089c403111b9266bfce3652835143cf
Patrick77.7 <patric.ius@live.de>
2021-05-25 12:34:19 +0200

    #Anlegen erster Test

---
+++ src/test/java/main/java/LeadTest.java
@@ -0,0 +1,13 @@
+package main.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeadTest {
+
+    @Test
+    void createNewLead() {
+        System.out.println("Test.");
+    }
+}
