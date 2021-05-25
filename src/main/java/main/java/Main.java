c78712cb4575b960f4b4d640fe3e9761909dafb3
Sebastian Siegling <s.sgl@t-online.de>
2021-05-25 19:10:51 +0200

    Continued working on the convertLeadToOpportunity method / Main Cleanup

--- src/main/java/main/java/Main.java
+++ src/main/java/main/java/Main.java
@@ -4,11 +4,6 @@ package main.java;
 public class Main {
     public static void main(String[] args) {
 
-//        Menu m1 = new Menu();
-//        m1.showMenu();
-        Lead l1 = new Lead("Stebe", "ssseee", "sseeff","ss" );
-        Contact c1 = new Contact(l1);
-        Opportunity o1 = new Opportunity(c1, Products.FLATBED,12,Status.OPEN);
 
     }
 }
