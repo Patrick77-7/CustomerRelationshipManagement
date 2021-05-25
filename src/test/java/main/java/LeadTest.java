3774dbf9b2ac14c65e1cb8f3999f831d0ee49237
Patrick77.7 <patric.ius@live.de>
2021-05-25 19:31:00 +0200

    #Erstelle Test für Lead-Klasse: Testen von createID

--- src/test/java/main/java/LeadTest.java
+++ src/test/java/main/java/LeadTest.java
@@ -8,6 +8,11 @@ class LeadTest {
 
     @Test
     void createNewLead() {
-        System.out.println("Test.");
+        Lead lead1 = new Lead("name","03012030xxxx","vorname.nachname@dkb.de","DKB");
+        Lead lead2 = new Lead("name2","03012030yyyy","vorname2.nachname2@dkb.de","DKB");
+        Lead lead3 = new Lead("name3","03012030zzzz","vornam3.nachname3@dkb.de","DKB");
+        assertTrue(lead1.getLeadId().equals("L 1"));
+        assertTrue(lead2.getLeadId().equals("L 2"));
+        assertTrue(lead3.getLeadId().equals("L 3"));
     }
 }
